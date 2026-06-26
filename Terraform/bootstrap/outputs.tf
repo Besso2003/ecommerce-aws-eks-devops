b097b7d808f57e82637e77e7c574a3e37079c5a2
Besso2003 <Bassant.ali23@icloud.com>
2026-06-26 21:10:24 +0300

    added car to ecr private repos

--- Terraform/bootstrap/outputs.tf
+++ Terraform/bootstrap/outputs.tf
@@ -5,3 +5,8 @@ output "state_bucket_name" {
 output "dynamodb_table_name" {
   value = aws_dynamodb_table.terraform_locks.name
 }
+
+output "github_actions_role_arn" {
+  description = "IAM role ARN GitHub Actions assumes via OIDC to push images to ECR"
+  value       = aws_iam_role.github_actions_ecr_push.arn
+}
